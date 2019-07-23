f4b2123819637b12b1b5556c7806c29b418c773a
Yaswanth Chandra, D <d.yaswanth.chandra@accenture.com>
2019-07-23 18:01:47 +0530

    new file:   Data Structures/Data Structures/SingleLinkedList.cs

---
+++ Data Structures/Data Structures/Practice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Structures
+{
+    class Practice
+    {
+        Node start;
+        public Practice()
+        {
+            start = null;
+        }
+        public void InsertAtEnd(int data)
+        {
+            if(start == null){
+                Node temp = new Node(data);
+                start = temp;
+            }
+
+            Node p;
+            p = start;
+
+            while(p.link != null)
+            {
+                p = p.link;
+            }
+
+            Node temp1 = new Node(data);
+            p.link = temp1;
+        }
+    }
+}
